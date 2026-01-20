0abd84e727ff98696d438f93c45df0c25ee6011f
Mani <maninotmoney@Manis-MacBook-Pro.local>
2026-01-20 15:27:57 +0530

    Remove individual website folders

--- gmb-3/src/components/FloatingWhatsapp.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-const FloatingWhatsapp = () => {
-    return (
-        <a href="#" className="float-whatsapp animate slide-in-left">
-            <i className="fab fa-whatsapp"></i>
-        </a>
-    );
-};
-export default FloatingWhatsapp;
