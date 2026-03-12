21cbb46488ddd5a283b552b07d01d1b219a9dea8
kbmeta <seow65521@gmail.com>
2026-03-12 14:09:37 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,54 +1,62 @@
 import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Header from './components/Header';
-import Hero from './components/Hero';
-import IntroSection from './components/IntroSection';
-import SportsSection from './components/SportsSection';
-import CasinoSection from './components/CasinoSection';
-import LiveSection from './components/LiveSection';
-import PromoBanner from './components/PromoBanner';
-import PlatformTable from './components/PlatformTable';
-import LegalSection from './components/LegalSection';
-import StepsSection from './components/StepsSection';
-import BonusesSection from './components/BonusesSection';
-import PaymentProof from './components/PaymentProof';
-import FAQSection from './components/FAQSection';
 import Footer from './components/Footer';
 import WhatsAppFloat from './components/WhatsAppFloat';
-
+import SEOKeywords from './components/SEOKeywords';
+import Home from './pages/Home';
+import AboutUs from './pages/AboutUs';
+import Services from './pages/Services';
+import Platforms from './pages/Platforms';
+import BlogPage from './pages/BlogPage';
+import ReferralCode from './pages/ReferralCode';
+import Cricket from './pages/Cricket';
+import Football from './pages/Football';
+import Tennis from './pages/Tennis';
+import LiveCricketBetting from './pages/LiveCricketBetting';
+import Kabaddi from './pages/Kabaddi';
+import HorseRaceBetting from './pages/HorseRaceBetting';
+import FantasySports from './pages/FantasySports';
+import Casino from './pages/Casino';
+import RulesRegulations from './pages/RulesRegulations';
+import HowToPlay from './pages/HowToPlay';
+import RefundPolicy from './pages/RefundPolicy';
+import PrivacyPolicy from './pages/PrivacyPolicy';
+import Disclaimer from './pages/Disclaimer';
 function App() {
   return (
-    <div className="app-container">
-      <Header />
-      <div id="home">
-        <Hero />
-      </div>
+    <Router>
+      <div className="app-container">
+        <Header />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/home" element={<Home />} />
+          <Route path="/about-us" element={<AboutUs />} />
+          <Route path="/services" element={<Services />} />
+          <Route path="/platforms" element={<Platforms />} />
+          <Route path="/blogs" element={<BlogPage />} />
 
-      <div className="container">
-        <IntroSection />
-        <div id="sports">
-          <SportsSection />
-        </div>
-        <div id="casino">
-          <CasinoSection />
-        </div>
-        <div id="live">
-          <LiveSection />
-        </div>
-        <div id="promotions">
-          <PromoBanner />
-          <BonusesSection />
-        </div>
-        <PlatformTable />
-        <LegalSection />
-        <StepsSection />
-        <PaymentProof />
-        <FAQSection />
+          {/* <Route path="/app-download" element={<AppDownload />} /> */}
+          <Route path="/referral" element={<ReferralCode />} />
+          <Route path="/cricket-betting" element={<Cricket />} />
+          <Route path="/football-betting" element={<Football />} />
+          <Route path="/tennis-betting" element={<Tennis />} />
+          <Route path="/live-cricket" element={<LiveCricketBetting />} />
+          <Route path="/kabaddi-betting" element={<Kabaddi />} />
+          <Route path="/horse-race-betting" element={<HorseRaceBetting />} />
+          <Route path="/fantasy-sports" element={<FantasySports />} />
+          <Route path="/live-casino" element={<Casino />} />
+          <Route path="/rules" element={<RulesRegulations />} />
+          <Route path="/how-to-play" element={<HowToPlay />} />
+          <Route path="/refund-policy" element={<RefundPolicy />} />
+          <Route path="/privacy-policy" element={<PrivacyPolicy />} />
+          <Route path="/disclaimer" element={<Disclaimer />} />
+        </Routes>
+        <SEOKeywords />
+        <Footer />
+        <WhatsAppFloat />
       </div>
-
-      <Footer />
-      <WhatsAppFloat />
-    </div>
+    </Router>
   );
 }
-
 export default App;
