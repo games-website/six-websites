f0386c3fa7fd4521623b23b46a547b3c4ca9611a
kbmeta <seow65521@gmail.com>
2026-04-11 01:16:01 +0530

    Delete src/components directory

--- src/components/WhatsAppFloat.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-const WhatsAppFloat = () => {
-    return (
-        <a href="https://wa.me/15208005239?text=Hello%20Lotus365%2C%20I%20want%20to%20get%20my%20ID." target="_blank" rel="noopener noreferrer" className="whatsapp-float" style={{ textDecoration: 'none', color: '#fff' }}>
-            <span>💬</span> Get ID Now
-        </a>
-    );
-};
-export default WhatsAppFloat;
