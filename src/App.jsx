27c2d53eaa5553a4d0452aab3dec117ec50b5241
kbmeta <seow65521@gmail.com>
2026-04-10 19:36:15 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -9,6 +9,23 @@ import CricketId from './pages/CricketId';
 import CasinoId from './pages/CasinoId';
 import Partners from './pages/Partners';
 import Services from './pages/Services';
+import FantasySports from './pages/FantasySports';
+import FeaturesPage from './pages/FeaturesPage';
+import FootballBetting from './pages/FootballBetting';
+import HorseRaceBetting from './pages/HorseRaceBetting';
+import HowToPlay from './pages/HowToPlay';
+import KabaddiBetting from './pages/KabaddiBetting';
+import LiveCasino from './pages/LiveCasino';
+import LiveCricket from './pages/LiveCricket';
+import PrivacyPolicy from './pages/PrivacyPolicy';
+import RefundPolicy from './pages/RefundPolicy';
+import Rules from './pages/Rules';
+import TennisBetting from './pages/TennisBetting';
+import AboutUs from './pages/AboutUs';
+import Blogs from './pages/Blogs';
+import Contact from './pages/Contact';
+import CricketBetting from './pages/CricketBetting';
+import Disclaimer from './pages/Disclaimer';
 const useScrollAnimation = () => {
   const location = useLocation();
   useEffect(() => {
@@ -47,6 +64,23 @@ function App() {
         <Route path="/casino-id" element={<CasinoId />} />
         <Route path="/partners" element={<Partners />} />
         <Route path="/services" element={<Services />} />
+        <Route path="/about-us" element={<AboutUs />} />
+        <Route path="/blogs" element={<Blogs />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/cricket-betting" element={<CricketBetting />} />
+        <Route path="/disclaimer" element={<Disclaimer />} />
+        <Route path="/fantasy-sports" element={<FantasySports />} />
+        <Route path="/features" element={<FeaturesPage />} />
+        <Route path="/football-betting" element={<FootballBetting />} />
+        <Route path="/horse-race-betting" element={<HorseRaceBetting />} />
+        <Route path="/how-to-play" element={<HowToPlay />} />
+        <Route path="/kabaddi-betting" element={<KabaddiBetting />} />
+        <Route path="/live-casino" element={<LiveCasino />} />
+        <Route path="/live-cricket" element={<LiveCricket />} />
+        <Route path="/privacy-policy" element={<PrivacyPolicy />} />
+        <Route path="/refund-policy" element={<RefundPolicy />} />
+        <Route path="/rules" element={<Rules />} />
+        <Route path="/tennis-betting" element={<TennisBetting />} />
       </Routes>
       <Footer />
       <FloatingWhatsapp />
