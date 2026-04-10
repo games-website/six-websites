028f8f292f2d2cef41665a47c7722a6f66d74fd0
kbmeta <seow65521@gmail.com>
2026-04-10 17:14:12 +0530

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,24 @@
-/** @type {import('tailwindcss').Config} */
+
 export default {
   content: [
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        brand: {
+          gold: '#bf953f',
+          'gold-light': '#fcf6ba',
+          'gold-dark': '#8a642d',
+          'gold-brilliant': '#b38728',
+        }
+      },
+      backgroundImage: {
+        'gold-gradient': 'linear-gradient(to right, #bf953f, #fcf6ba, #b38728, #fcf6ba, #bf953f)',
+        'gold-gradient-vertical': 'linear-gradient(to bottom, #bf953f, #fcf6ba, #b38728)',
+      }
+    },
   },
   plugins: [],
 }
